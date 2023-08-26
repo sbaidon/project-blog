f4b418cb759d5788a31327eb7bde6a7d605f80b8
Sergio Baidon <sbaidon@Sergios-MBP.attlocal.net>
2023-08-26 15:24:48 -0700

    generate blog posts statically

--- src/app/[postSlug]/page.tsx
+++ src/app/[postSlug]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import BlogHero from "@/components/blog-hero";
 
 import styles from "./postSlug.module.css";
-import { loadBlogPost } from "@/helpers/file-helpers";
+import { loadBlogPost, getBlogPostList } from "@/helpers/file-helpers";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import CodeSnippet from "@/components/code-snippet/code-snippet";
 import { notFound } from "next/navigation";
@@ -43,6 +43,15 @@ const components = {
   DivisionGroupsDemo: (props) => <DivisionGroupsDemo {...props} />,
   CircularColorsDemo: (props) => <CircularColorsDemo {...props} />,
 };
+
+export async function generateStaticParams() {
+  const posts = await getBlogPostList();
+  return posts.map(post => ({
+      params: {
+        slug: post.slug
+      }
+    }))
+}
 
 async function BlogPost({ params }: Props) {
   const post = await loadBlogPost(params.postSlug);
