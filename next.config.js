e5b8245daf0610588517f5eae308fac29a9672fb
Sergio Baidon Carrillo <sergca@amazon.com>
2023-08-31 18:37:16 -0700

    fix experimental

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
-const withNextIntl = require('next-intl/plugin')(
+const withNextIntl = require("next-intl/plugin")(
   // This is the default (also the `src` folder is supported out of the box)
-  './src/i18n.ts'
+  "./src/i18n.ts"
 );
 
 module.exports = withNextIntl({
   experimental: {
-    typedRoutes: true,
-    instrumentationHook: true,
-  },
-  experimental: {
     outputFileTracingIncludes: {
+      typedRoutes: true,
+      instrumentationHook: true,
       "/*": ["./content/**/*"],
     },
   },
