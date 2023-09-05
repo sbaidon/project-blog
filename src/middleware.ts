11af95f9ace7430e6a952404d80c7d3201951b6f
Sergio Baidon <sbaidon@Sergios-MBP.attlocal.net>
2023-09-04 20:00:30 -0700

    Remove apple touch icon middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,6 +27,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico|rss.xml|site.webmanifest).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|rss.xml|site.webmanifest|apple-touch-icon.png).*)",
   ],
 };
